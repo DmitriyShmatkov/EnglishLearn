99184d2bc9e06dd42a10275b3eda30bab95e6b0a
Dmitriy Shmatkov <chmatikd36@gmail.com>
2021-05-28 18:18:39 +0300

    Added mini-game fragment layout

---
+++ app/src/main/java/ua/opu/englishlearn/main/fragments/MiniGameFragment.java
@@ -0,0 +1,27 @@
+package ua.opu.englishlearn.main.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import ua.opu.englishlearn.R;
+
+public class MiniGameFragment extends Fragment {
+
+    public MiniGameFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_mini_game, container, false);
+    }
+}
---
+++ app/src/main/java/ua/opu/englishlearn/main/fragments/StatisticsFragment.java
@@ -0,0 +1,27 @@
+package ua.opu.englishlearn.main.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import ua.opu.englishlearn.R;
+
+public class StatisticsFragment extends Fragment {
+
+    public StatisticsFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_statistics, container, false);
+    }
+}
