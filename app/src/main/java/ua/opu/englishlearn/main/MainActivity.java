3d5190f31dc13e182b5b1a32d890330b5203f841
Dmitriy Shmatkov <chmatikd36@gmail.com>
2021-05-29 22:14:14 +0300

    Added navigation between vocabulary and words add fragments

--- app/src/main/java/ua/opu/englishlearn/main/MainActivity.java
+++ app/src/main/java/ua/opu/englishlearn/main/MainActivity.java
@@ -1,20 +1,33 @@
 package ua.opu.englishlearn.main;
 
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
-import android.os.Bundle;
-import android.widget.Button;
-import android.widget.ImageButton;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
 import ua.opu.englishlearn.R;
+import ua.opu.englishlearn.main.fragments.StatisticsFragment;
+import ua.opu.englishlearn.main.fragments.VocabularyFragment;
+import ua.opu.englishlearn.main.fragments.WordAddFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActionBar actionBar;
+    List<ImageButton> bottomNavButtons;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,13 +36,88 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar = findViewById(R.id.mainActivityToolbar);
         setSupportActionBar(toolbar);
-        ActionBar actionBar = Objects.requireNonNull(getSupportActionBar());
+        actionBar = Objects.requireNonNull(getSupportActionBar());
         actionBar.setTitle(R.string.vocabulary_fragment_title);
 
-        List<ImageButton> bottomNavButtons = new ArrayList<>();
-        bottomNavButtons.add(findViewById(R.id.vocabularyButton));
-        bottomNavButtons.add(findViewById(R.id.statsButton));
+        ViewPager2 viewPager = findViewById(R.id.mainViewPager);
+        MainViewPagerAdapter viewPagerAdapter = new MainViewPagerAdapter(this);
+        viewPagerAdapter.addFragment(new VocabularyFragment(0, viewPagerAdapter));
+        viewPagerAdapter.addFragment(new StatisticsFragment());
+        viewPager.setAdapter(viewPagerAdapter);
+        viewPager.setUserInputEnabled(false);
+        viewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                for (ImageButton bottomButton : bottomNavButtons) {
+                    if (bottomNavButtons.indexOf(bottomButton) == position) {
+                        bottomButton.setColorFilter(Color.WHITE);
+                    } else {
+                        bottomButton.setColorFilter(Color.BLACK);
+                    }
+                }
+            }
+        });
 
+        ImageButton vocabularyBottomButton = findViewById(R.id.vocabularyButton);
+        ImageButton statisticsBottomButton = findViewById(R.id.statsButton);
+        bottomNavButtons = new ArrayList<>();
+        bottomNavButtons.add(vocabularyBottomButton);
+        bottomNavButtons.add(statisticsBottomButton);
 
+        vocabularyBottomButton.setOnClickListener(v -> {
+            actionBar.setTitle(R.string.vocabulary_fragment_title);
+            viewPager.setCurrentItem(0, false);
+        });
+        statisticsBottomButton.setOnClickListener(v -> {
+            actionBar.setTitle(R.string.statistics_fragment_title);
+            viewPager.setCurrentItem(1, false);
+        });
+
+    }
+
+    public class MainViewPagerAdapter extends FragmentStateAdapter {
+
+        private List<Fragment> viewPagerFragments;
+
+        public MainViewPagerAdapter(@NonNull FragmentActivity fragmentActivity) {
+            super(fragmentActivity);
+            viewPagerFragments = new ArrayList<>();
+        }
+
+        public void addFragment(Fragment fragment) {
+            viewPagerFragments.add(fragment);
+        }
+
+        @NonNull
+        @Override
+        public Fragment createFragment(int position) {
+            return viewPagerFragments.get(position);
+        }
+
+        @Override
+        public int getItemCount() {
+            return viewPagerFragments.size();
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return viewPagerFragments.get(position).hashCode();
+        }
+
+        @Override
+        public boolean containsItem(long itemId) {
+            for (Fragment fragment : viewPagerFragments) {
+                if (fragment.hashCode() == itemId) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void replace(int position, Fragment fragment) {
+            viewPagerFragments.set(position, fragment);
+            notifyItemChanged(position);
+        }
     }
 }
--- app/src/main/java/ua/opu/englishlearn/main/fragments/VocabularyFragment.java
+++ app/src/main/java/ua/opu/englishlearn/main/fragments/VocabularyFragment.java
@@ -1,18 +1,33 @@
 package ua.opu.englishlearn.main.fragments;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+
+import java.util.Objects;
+
 import ua.opu.englishlearn.R;
+import ua.opu.englishlearn.main.MainActivity;
 
 public class VocabularyFragment extends Fragment {
 
+    private int position;
+    private MainActivity.MainViewPagerAdapter viewPagerAdapter;
+
     public VocabularyFragment() {
+    }
+
+    public VocabularyFragment(int position, MainActivity.MainViewPagerAdapter viewPagerAdapter) {
+        this.position = position;
+        this.viewPagerAdapter = viewPagerAdapter;
     }
 
     @Override
@@ -24,5 +39,25 @@ public class VocabularyFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_vocabulary, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        view.findViewById(R.id.wordAddButton).setOnClickListener(v -> {
+            AppCompatActivity activity = Objects.requireNonNull((AppCompatActivity) getActivity());
+            ActionBar actionBar = Objects.requireNonNull(activity.getSupportActionBar());
+            actionBar.setTitle(R.string.word_add_fragment_title);
+            Toolbar toolbar = activity.findViewById(R.id.mainActivityToolbar);
+
+            toolbar.setNavigationIcon(R.drawable.ic_baseline_arrow_back_24);
+            toolbar.setNavigationOnClickListener(v1 -> {
+                actionBar.setTitle(R.string.vocabulary_fragment_title);
+                toolbar.setNavigationIcon(null);
+                viewPagerAdapter.replace(position, this);
+            });
+            viewPagerAdapter.replace(position, new WordAddFragment());
+        });
     }
 }
